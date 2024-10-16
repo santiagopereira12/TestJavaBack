f5317d609927a212f125b0b1beae5e5f3609804e
santiagopereira12 <spereira98@misena.edu.co>
2024-10-16 18:46:09 -0500

    Retornar un modelo para crear el usuario

--- src/main/java/com/project/test/test/controller/UserController.java
+++ src/main/java/com/project/test/test/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.project.test.test.controller;
 
 import com.project.test.test.models.requests.UserRegisterRequestModel;
+import com.project.test.test.models.responses.UserRest;
 import com.project.test.test.persistence.entities.UserEntity;
 import com.project.test.test.services.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +20,10 @@ public class UserController {
     UserService userService;
 
     @PostMapping
-    public UserEntity createUser(@RequestBody @Valid UserRegisterRequestModel userModel) {
+    public UserRest createUser(@RequestBody @Valid UserRegisterRequestModel userModel) {
         UserEntity user = userService.createUser(userModel);
-        return user;
+        UserRest userRest = new UserRest();
+        BeanUtils.copyProperties(user, userRest);
+        return userRest;
     }
 }
---
+++ src/main/java/com/project/test/test/models/responses/UserRest.java
@@ -0,0 +1,10 @@
+package com.project.test.test.models.responses;
+
+import lombok.Data;
+
+@Data
+public class UserRest {
+    private Integer idUser;
+    private String username;
+    private String emailUser;
+}
